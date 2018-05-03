a2a96394d210f5500637480a916a9eda4a3c4d91
Erkut <tulunayerkut@gmail.com>
2018-05-04 00:57:20 +0200

    added playlist

--- src/App.js
+++ src/App.js
@@ -4,11 +4,34 @@ import './App.css';
 let defaultStyle={
   color: '#000'
 }
+let fakeServerData ={
+  user:{
+    name: 'Erkut',
+    playlists: [
+      {
+        name: 'Playlist-1',
+        songs: ['Sypmhony of Destruction', 'Tornado of Souls', 'Holy wars', 'Whisky in The Jar']
+      },
+      {
+        name: 'Playlist-2',
+        songs: ['Sypmhony of Destruction', 'Tornado of Souls', 'Holy wars', 'Whisky in The Jar']
+      },
+      {
+        name: 'Playlist-3',
+        songs: ['Sypmhony of Destruction', 'Tornado of Souls', 'Holy wars', 'Whisky in The Jar']
+      },
+      {
+        name: 'Playlist-4',
+        songs: ['Sypmhony of Destruction', 'Tornado of Souls', 'Holy wars', 'Whisky in The Jar']
+      }
+    ]
+  }
+}
 class Aggregate extends Component {
   render(){
       return (
         <div style={{width: '40%', display:'inline-block'}}>
-          <h2 style={defaultStyle}>Number Text</h2>
+          <h2 style={defaultStyle}>{this.props.playlists && this.props.playlists.length} Text</h2>
         </div>
       );
   }
@@ -43,18 +66,34 @@ class Playlist extends Component{
 }
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state={serverData: {}}
+  }
+
+  componentDidMount(){
+      setTimeout(()=> {
+      this.setState({serverData: fakeServerData});
+    }, 1000);
+  }
   render() {
     return (
+
       <div className="App">
-      <h1>Title</h1>
-      <Aggregate/>
-      <Aggregate/>
+        {this.state.serverData.user ?
+    <div>
+        <h1>{this.state.serverData.user.name}'s Playlists</h1>    
+        <Aggregate playlists={this.state.serverData.user.playlists}/>
+        <Aggregate/>  
       <Filter/>
       <Playlist/>
       <Playlist/>
       <Playlist/>
       <Playlist/>
       </div>
+      : <h1>Loading...</h1>
+       }
+       </div>
     );
   }
 }
